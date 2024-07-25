896e726a84a1002b5bd4254931a622626167b88f
Muanya <nwokejichimuanya@gmail.com>
2024-07-25 17:57:49 +0100

    updated email auth page

--- lib/src/bloc/authentication_bloc/auth_states.dart
+++ lib/src/bloc/authentication_bloc/auth_states.dart
@@ -9,3 +9,7 @@ class InitialAuthState extends AmptiveAuthState{}
 class MainAuthState extends AmptiveAuthState{
   MainAuthState({super.userEmail});
 }
+
+class LoadingAuthState extends AmptiveAuthState {}
+
+class LoadedAuthState extends AmptiveAuthState {}
