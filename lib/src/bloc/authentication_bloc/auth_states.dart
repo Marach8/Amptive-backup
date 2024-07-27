2eb3827a814291e5393d16db483a06d79ce8e4e5
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-07-27 11:01:20 +0100

    Resolved conficts

--- lib/src/bloc/authentication_bloc/auth_states.dart
+++ lib/src/bloc/authentication_bloc/auth_states.dart
@@ -35,3 +35,7 @@ class InitialAuthState extends AmptiveAuthState{}
 class MainAuthState extends AmptiveAuthState{
   MainAuthState({super.userEmail});
 }
+
+class LoadingAuthState extends AmptiveAuthState{}
+
+class LoadedAuthState extends AmptiveAuthState{}
--- lib/src/views/screens/main_application_screens/preference_screen.dart
+++ lib/src/views/screens/main_application_screens/preference_screen.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
-
 import 'package:amptive/src/utils/constants/colors.dart';
 import 'package:amptive/src/views/widgets/common_widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-
 import '../../../providers/preference_provider.dart';
 
 
