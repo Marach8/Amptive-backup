960eecf4ec4e9c45bd0b5335a1157b82257ddba0
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2026-06-08 08:11:34 +0100

    chore

--- lib/src/config/routing/route_transition.dart
+++ lib/src/config/routing/route_transition.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:amptive/src/config/config_export.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
