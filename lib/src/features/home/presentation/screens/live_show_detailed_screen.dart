86aa02287033f0768fe5d485fc36eeb04b795090
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2026-07-08 16:36:21 +0100

    chore

--- lib/src/features/home/presentation/screens/live_show_detailed_screen.dart
+++ lib/src/features/home/presentation/screens/live_show_detailed_screen.dart
@@ -15,7 +15,6 @@ import 'package:amptive/src/shared/annotated_region_widget.dart';
 import 'package:amptive/src/shared/global_model_objects.dart';
 import 'package:amptive/src/shared/image_loader_widget.dart';
 import 'package:amptive/src/shared/live_indicators.dart';
-import 'package:amptive/src/shared/row_of_people_listening_widget.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:readmore/readmore.dart';
