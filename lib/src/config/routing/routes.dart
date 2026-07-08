714ea22f975378279ad34014c78702e2268bb9cf
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2026-07-08 17:30:41 +0100

    chore

--- lib/src/config/routing/routes.dart
+++ lib/src/config/routing/routes.dart
@@ -92,7 +92,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 final GoRouter amptiveAppRouter = GoRouter(
   navigatorKey: navigatorKey,
   initialLocation: ATRoutes.onboardingScreen.addSlash,
-  //redirect: tgRedirect,
+  redirect: tgRedirect,
 
   routes: <RouteBase>[
     GoRoute(
